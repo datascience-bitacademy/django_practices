f6ca24ee4c156f44585e2f851267c976b1410e5f
datascience-bitacademy <datascience.bitacademy@gmail.com>
2020-09-04 15:35:55 +0900

    sql 추가

---
+++ sql/emaillist.sql
@@ -0,0 +1,6 @@
+desc emaillist;
+
+select no, first_name, last_name, email from emaillist;
+
+insert emaillist values(null, '안', '대혁', 'kickscar@gmail.com');
+insert emaillist values(null, '둘', '리', 'dooly@gmail.com');
